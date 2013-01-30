5cb10811b2a17af1449ed808226ded34badc493a
Yev <evg1985@gmail.com>
2013-01-30 17:45:41 +0200

    + missed src

---
+++ dev/project/src/Utils.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "Utils.h"
+
+namespace Utils
+{
+	void ReplaceString(std::string& str, const std::string& toReplace, const std::string& replaceWith)
+	{
+		std::string::size_type pos = 0;
+
+		while ((pos = str.find(toReplace)) != std::string::npos)
+		{
+			str.replace(pos, toReplace.size( ), replaceWith);
+			pos += replaceWith.size( );
+		}
+	}
+
+	void SplitString(const std::string &in, const std::string &delim, std::vector<std::string> &out) 
+	{
+		
+		std::string::size_type beg = 0;
+		std::string::size_type end = in.find(delim);
+
+		while (end > beg)
+		{
+			size_t count = end - beg;
+			out.push_back(in.substr(beg, count));
+			end += delim.size();
+			beg = end;
+
+			end = in.find(delim, beg);
+			if (end == std::string::npos)
+			{
+				end = in.length();
+			}
+		}
+	}
+} // Utils
---
+++ dev/project/src/Utils.h
@@ -0,0 +1,14 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <string>
+#include <vector>
+
+namespace Utils
+{
+	// replaces substring toReplace by string replaceWith
+	void ReplaceString(std::string& str, const std::string& toReplace, const std::string& replaceWith);
+	void SplitString(const std::string &in, const std::string &delim, std::vector<std::string> &out);
+} // Utils
+
+#endif // _UTILS_H_
